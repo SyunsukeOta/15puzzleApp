b83003945be35b7e1d6563ba847f9b4b72fd7c4e
kibaota <ohta.syunsuke@liveout.co.jp>
2020-04-13 11:53:36 +0900

    moveCountの変更

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const app = new Vue({
   data: {
     color: "green",
     direction: ["left", "right", "top", "bottom"],
-    moveCount: 50,
+    moveCount: 10000,
     blockData: [
       [
         { id: 0, color: "blue", number: 1 },
